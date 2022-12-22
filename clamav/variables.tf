fca709d023141bf57d1a582b8f3ffbcf4b28d60d
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-12-22 12:03:33 -0500

    Correct the clamav app_name_or_id description

--- clamav/variables.tf
+++ clamav/variables.tf
@@ -10,7 +10,7 @@ variable "cf_space_name" {
 
 variable "app_name_or_id" {
   type        = string
-  description = "base application name to be accessed at this domain name"
+  description = "base application name to allow routing to the clamav app"
 }
 
 variable "name" {
