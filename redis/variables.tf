32dc4c331da2a32e90cc90374d38a8a64f564121
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-03-18 10:13:16 -0400

    Add json_params for redis

--- redis/variables.tf
+++ redis/variables.tf
@@ -29,3 +29,10 @@ variable "tags" {
   type        = list(string)
   default     = []
 }
+
+variable "json_params" {
+  description = "A JSON string of arbitrary parameters"
+  type        = map(object)
+  default     = false
+  # See options at https://cloud.gov/docs/services/aws-elasticache/#setting-optional-parameters
+}
