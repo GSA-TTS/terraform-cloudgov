541d295c6da7bcb073cabfd261df31c34439d3b3
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-05-08 15:00:47 -0400

    Optionally allow specifying 0:n target apps for domain service

--- domain/variables.tf
+++ domain/variables.tf
@@ -10,7 +10,14 @@ variable "cf_space_name" {
 
 variable "app_name_or_id" {
   type        = string
-  description = "base application name to be accessed at this domain name"
+  description = "base application name or id to be accessed at this domain name. Conflicts with var.app_names_or_ids"
+  default     = null
+}
+
+variable "app_names_or_ids" {
+  type        = list(string)
+  description = "base application names or ids to be accessed at this domain name. Overwritten by var.app_name_or_id"
+  default     = []
 }
 
 variable "name" {
