95bf1f6d00e01fa039f530eafea27a12a92db715
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-12 12:29:21 -0400

    cleanup script

--- application/prepare_app.sh
+++ application/prepare_app.sh
@@ -17,11 +17,8 @@ curl -s -L "https://github.com/${ORG}/${REPO}/archive/${GITREF}.zip" --output lo
 
 zip_folder=$(unzip -l local.zip | awk '/\/$/ {print $4}' | awk -F'/' '{print $1}' | sort -u)
 # Zip up just the $REPO-$branch/ subdirectory for pushing
-# Before zip stage, run [ npm ci --production | npm run build ] in /backend/ to get the compiled assets for the site in /static/compiled/
 unzip -q -u local.zip "$zip_folder/$SRC_FOLDER/*"
 cd "$zip_folder/$SRC_FOLDER/" &&
-# npm ci --production --silent --no-progress &&
-# npm run build > '/dev/null' 2>&1 &&
 zip -q -r -o -X "${popdir}/app.zip" ./
 
 # Tell Terraform where to find it
