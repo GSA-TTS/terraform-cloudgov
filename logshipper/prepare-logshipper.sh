26cd97fd09525b459dde765552df6b763dddda75
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-19 09:35:52 -0400

    Fix prepare script to account for different branches

--- logshipper/prepare-logshipper.sh
+++ logshipper/prepare-logshipper.sh
@@ -4,8 +4,7 @@
 set -e
 
 popdir="$(pwd)"
-#eval "$(jq -r '@sh "GITREF=\(.gitref)"')"
-GITREF='refs/heads/main'
+eval "$(jq -r '@sh "GITREF=\(.gitref)"')"
 
 # Portable construct so this will work everywhere
 # https://unix.stackexchange.com/a/84980
@@ -15,12 +14,13 @@ cd "${tmpdir}"
 # Grab a copy of the zip file for the specified ref
 # https://github.com/GSA-TTS/cg-logshipper/archive/refs/heads/main.zip
 curl -s -L https://github.com/GSA-TTS/cg-logshipper/archive/"${GITREF}".zip --output "${tmpdir}/logshipper-dist.zip"
+branch=$(echo "$GITREF" | cut -f3 -d"/")
 
 # Remove the leading directory; the .zip needs to have the files at the top
 cd "${tmpdir}"
 unzip -o "${tmpdir}/logshipper-dist.zip" > /dev/null
 cat "${popdir}/fluentbit_config/fluentbit.conf" > "${tmpdir}/cg-logshipper-main/fluentbit.conf"
-cd "${tmpdir}/cg-logshipper-main" && zip -r -o -X "${popdir}/logshipper.zip" ./ > /dev/null
+cd "${tmpdir}/cg-logshipper-${branch}" && zip -r -o -X "${popdir}/logshipper.zip" ./ > /dev/null
 
 # Tell Terraform where to find it
 cat << EOF
