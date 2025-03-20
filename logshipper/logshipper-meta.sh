0f19707cb7a6434684312577e5fd5a0e03c7fba1
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-20 12:18:43 -0400

    Remove the s3 and offload it from the module

--- logshipper/logshipper-meta.sh
+++ logshipper/logshipper-meta.sh
@@ -6,7 +6,7 @@ org="$1"; space="$2";
 http_user="$3"; http_pass="$4";
 nr_key="$5"; nr_api="$6";
 logdrain="$7"; app_name="$8";
-s3_name="$9";
+# s3_name="$9";
 
 # Since cf cups does not support --wait, we use this as a basic way to ensure
 # that the api has caught up with things.
@@ -31,7 +31,7 @@ bind_services() {
     cf bind-service "$app_name" logshipper-creds --wait &&
     cf bind-service "$app_name" logshipper-new-relic-creds --wait &&
     cf bind-service "$app_name" logdrain --wait &&
-    cf bind-service "$app_name" "$s3_name" --wait &&
+    # cf bind-service "$app_name" "$s3_name" --wait &&
     cf restage "$app_name" > /dev/null 2>&1
 }
 
