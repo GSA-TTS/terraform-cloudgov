092ab2ebcd529060637a0b9ecb0319210dacc2ee
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-03-24 14:16:10 -0400

    Protect against globbing/splitting

--- egress_proxy/prepare-proxy.sh
+++ egress_proxy/prepare-proxy.sh
@@ -5,7 +5,7 @@ set -e
 
 eval "$(jq -r '@sh "GITREF=\(.gitref)"')"
 
-popdir=$(pwd)
+popdir="$(pwd)"
 
 # Portable construct so this will work everywhere
 # https://unix.stackexchange.com/a/84980
@@ -13,11 +13,11 @@ tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 cd "$tmpdir"
 
 # Grab a copy of the zip file for the specified ref
-curl -s -L https://github.com/GSA-TTS/cg-egress-proxy/archive/${GITREF}.zip --output local.zip
+curl -s -L "https://github.com/GSA-TTS/cg-egress-proxy/archive/${GITREF}.zip" --output local.zip
 
 # Zip up just the proxy/ subdirectory for pushing
 unzip -q -u local.zip \*/proxy/\*
-zip -q -j -r ${popdir}/proxy.zip cg-egress-proxy-*/proxy
+zip -q -j -r "${popdir}"/proxy.zip cg-egress-proxy-*/proxy
 
 # Tell Terraform where to find it
 cat << EOF
