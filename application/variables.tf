916aa3aa2a97710b4adc2770458d14e18928db94
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-03-24 10:59:04 -0400

    Allow specifying a full app route

--- application/variables.tf
+++ application/variables.tf
@@ -45,6 +45,12 @@ variable "name" {
   type        = string
 }
 
+variable "route" {
+  description = "The full app route. Defaults to {var.name}.app.cloud.gov if this is omitted"
+  type        = string
+  default     = null
+}
+
 variable "gitref" {
   type        = string
   description = "gitref for the specific version of applicaion that you want to use. Branch name should start with `refs/heads` while a git sha should be given without a prefix"
