e0104c50497a7f3da5a73248114465ac7865d90a
Christopher Bisom <chris.bisom@gsa.gov>
2026-04-06 12:53:38 -0400

    pass enable_hsts to spiff

--- spiffworkflow/variables.tf
+++ spiffworkflow/variables.tf
@@ -66,6 +66,12 @@ variable "global_scripts_dir" {
   default     = ""
 }
 
+variable "enable_hsts" {
+  type        = bool
+  description = "set to true to require HSTS headers on all backend responses"
+  default     = false
+}
+
 ###############################################################################
 # Backend Variables
 ###############################################################################
